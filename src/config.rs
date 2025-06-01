0a120ffc3688e750287f081057177c2fb46fe5b1
macsur <macttnk@gmail.com>
2025-06-02 07:46:54 +0800

    修改中继服务器域名和key

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.136777.xyz"];
+pub const RS_PUB_KEY: &str = "nOSh65GQ01LPK4GQy+GthrHpL7qWXhV3WTyCuNkhoXQ=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
